cb015cfbf243c3fd919b6a1f40ef205ec87a8a4d
Elena <puchochek@gmail.com>
2019-02-27 21:25:58 +0300

    Separated endpoints added.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, Post, Body, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Expence } from './app.entity';
 import { NewExpence } from './expence.dto';
 
 
-@Controller()
+@Controller('expences')
 export class AppController {
   constructor(private appService: AppService) {}
 
@@ -18,17 +18,22 @@ export class AppController {
     return this.appService.getExpences();
   }
 
+ @Get(':category')
+ getExpenceByCategory(@Param('category') category): Promise<Expence[]> {
+  console.log(`${category}`);
+
+  return this.appService.getExpenceByCategory(category);
+ }
+
   @Post()
   async createNewExpence(@Body() newExpence: NewExpence): Promise<Expence[]> {
     const expenceToSave = newExpence;
     expenceToSave.id = this.appService.getId();
     expenceToSave.date = this.appService.parseDate(newExpence.dateToParse);
-    
     const result: Expence [] = await this.appService.saveNewExpence(expenceToSave);
     if (!result) {
       console.log('Error');
     }
-   
     return result;
   }
 
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Expence } from './app.entity';
 import { getConnection } from 'typeorm';
-//import { getRepository } from 'typeorm';
+import { getRepository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { NewExpence } from './expence.dto';
@@ -12,50 +12,32 @@ export class AppService {
   constructor(
     @InjectRepository(Expence)
     private readonly expenceRepository: Repository<Expence>,
-  ) {
-    // const pr1 = new Expence();
-    //   pr1.id = 1;
-    //   pr1.sum = 8;
-    //   pr1.comment = 'from repository';
-    //   this.expenceRepository.save(pr1);
-  }
-
-  // getHello(): Object {
-  //   const response = {
-  //     message: 'Hello, Lena!'
-  //   };
-
-  //   return response;
-  // }
+  ) {}
 
   async getExpences() : Promise<Expence[]> {
-   
     return await this.expenceRepository.find();
-
   }
 
   async saveNewExpence(newExpence: any) : Promise<Expence[]> {
-   
     return await this.expenceRepository.save(newExpence);
   }
 
-  // async getData() {
-    
-  //   const expence = await getConnection()
-  //     .createQueryBuilder()
-  //     .select(['date', 'comment', 'type', 'sum'])
-  //     .from(Expence, "Expence")
-  //     .getOne();
-  //   console.log('response', expence);
-  //   return expence;
-  // }
-  getId() : string {
+  async getExpenceByCategory(category: string) {
+    const expence = await getRepository(Expence)
+    .createQueryBuilder('Expence')
+    .where('Expence.type = :type', { type: category })
+    .getMany();
+
+    console.log('response', expence);
+    return expence;
+  }
+
+  getId(): string {
     const uuidv4 = require('uuid/v4');
-    
     return uuidv4();
   }
 
-  parseDate(dateToparse : string) : Date {
+  parseDate(dateToparse: string): Date {
     const parsedDate =  dateToparse.split('.');
     const year = parsedDate[2].substring(0, 4);
     const month = parsedDate[1].length === 1 ?
@@ -66,8 +48,6 @@ export class AppService {
     : parsedDate[0];
 
     const date = new Date(year + '-' + month + '-' + day);
-  
     return date;
   }
-  
 }
