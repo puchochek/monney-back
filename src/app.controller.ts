53cfc580d65ed75952b65a13d319bfb52c521940
Elena <puchochek@gmail.com>
2019-12-23 09:09:54 +0300

    f

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'constants';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) {
+		{
 
-  @Get()
-  getHello(): string {
-    console.log('---> Hello, monney' );
-    return this.appService.getHello();
-  }
+			console.log('---> Hello, monney');
+
+
+
+		}
+	}
+
+	@Get()
+	getHello(): string {
+		return this.appService.getHello();
+	}
 }
